ebe5309a544e68b43bfcbea4739409f511220443
Joan Menendez Calafell <jmenendezc@alumnes.santjosepobrer.es>
2023-10-11 15:01:48 +0200

    dangit

--- Actividad 4/Actividad4.3.py
+++ Actividad 4/Actividad4.3.py
@@ -3,4 +3,7 @@ Bnonum = input("Establezca el valor de B: ")
 A = int(Anonum)
 B = int(Bnonum)
 if A > B: Respuesta = print(A, "es superior a", B)
-else: Respuesta = print(B, "es superior a", A)
+
+else: 
+    if B > A:Respuesta = print(B, "es superior a", A)
+    else: Respuesta = print(A, "y", B, "son iguales")
--- Actividad 4/Actividad4.4.py
+++ Actividad 4/Actividad4.4.py
@@ -1,9 +1,15 @@
 A = int(input("Establezca el valor de A: "))
 B = int(input("Establezca el valor de B: "))
 C = int(input("Establezca el valor de C: "))
-if A > B: 
-    if A > C: Respuesta = print(A, "es superior a", B, "y", C)
-    else: Respuesta = print(A, "es superior a", B, "pero inferior a", C)
-else: 
-    if B > C: Respuesta = print(B, "es superior a", A, "y", C)
-    else: Respuesta = print(B, "es superior a", A, "pero inferior a", C)
+if A == B and A == C: Respuesta = print("Los tres valores son iguales")
+else:
+    if A > B: 
+        if A > C: Respuesta = print(A, "es superior a", B, "y", C)
+        else: 
+            if C > A: Respuesta = print(A, "es superior a", B, "pero inferior a", C)
+            else: Respuesta = print(A, "es superior a", B, "pero igual a", C)
+    else: 
+        if B > C: Respuesta = print(B, "es superior a", A, "y", C)
+        else: 
+            if C > B: Respuesta = print(B, "es superior a", A, "pero inferior a", C)
+            else: Respuesta = print(B, "es superior a", A, "pero igual a", C)
