d996c367784f1ea4087a418f1b7e226ac3bc9fbb
Joan Menendez Calafell <jmenendezc@alumnes.santjosepobrer.es>
2023-10-26 16:32:45 +0200

    wow

--- Actividad 4/Actividad4.4.py
+++ Actividad 4/Actividad4.4.py
@@ -3,13 +3,20 @@ B = int(input("Establezca el valor de B: "))
 C = int(input("Establezca el valor de C: "))
 if A == B and A == C: Respuesta = print("Los tres valores son iguales")
 else:
-    if A > B: 
-        if A > C: Respuesta = print(A, "es superior a", B, "y", C)
-        else: 
-            if C > A: Respuesta = print(A, "es superior a", B, "pero inferior a", C)
-            else: Respuesta = print(A, "es superior a", B, "pero igual a", C)
-    else: 
-        if B > C: Respuesta = print(B, "es superior a", A, "y", C)
-        else: 
-            if C > B: Respuesta = print(B, "es superior a", A, "pero inferior a", C)
-            else: Respuesta = print(B, "es superior a", A, "pero igual a", C)
+    if A == B or A == C or B == C:
+        if A == B:
+            if A > C: Respuesta = print(A, "y", B, "son iguales y superiores a", C)
+            else: Respuesta = print(A, "y", B, "son iguales pero inferiores a", C)
+        if A == C: 
+            if A > B: Respuesta = print(A, "y", C, "son iguales y superiores a", B)
+            else: Respuesta = print(A, "y", C, "son iguales pero inferiores a", B)
+        if B == C:
+            if A > B: Respuesta = print(B, "y", C, "son iguales y superiores a", A)
+            else: Respuesta = print(B, "y", C, "son iguales pero inferiores a", A)
+    else:
+        if A > B:
+            if A > C: Respuesta = print(A, "es superior a", B, "y", C)
+            else: Respuesta = print(A, "es superior a", B, "pero inferior a", C)
+        if B > A:
+            if B > C: Respuesta = print(B, "es superior a", A, "y", C)
+            else: Respuesta = print(B, "es superior a", A, "pero inferior", C)
--- Actividad 6/Actividad6.4.py
+++ Actividad 6/Actividad6.4.py
@@ -1,9 +1,15 @@
 Num = 0
 Numers = 0
 Contador = 0
+Num = int(input("Escriba un número: "))
 while Num != -1:
-    Num = int(input("Escriba un número: "))
-    Contador = Contador + 1
-    Numers = Num + Numers
+    if Num < -1:
+        print("Valor no válido")
+        Num = int(input("Escriba un número: "))
+    else: 
+        if Num != -1:
+            Contador = Contador + 1
+            Numers = Num + Numers
+            Num = int(input("Escriba un número: "))
 Resultado = Numers / Contador
 print(Resultado)
