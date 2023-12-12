16a311907ae5526d501b2ddb285d56ee166fb798
Joan Menendez Calafell <jmenendezc@alumnes.santjosepobrer.es>
2023-12-12 17:35:10 +0100

    wowie

--- Space Invaders/objetos.py
+++ Space Invaders/objetos.py
@@ -7,7 +7,9 @@ class Cat:
 
         self.derecha = pygame.image.load("derecha.png")
         self.izquierda = pygame.image.load("izquierda.png")
-
+        
+        self.puede_disparar = True  # Nuevo atributo para controlar si puede disparar
+        
         scaled_width = 100
         scaled_height = 93
         self.derecha = pygame.transform.scale(self.derecha, (scaled_width, scaled_height))
@@ -35,3 +37,75 @@ class Cat:
         if tiempo_actual - self.tiempo_anterior_imagen >= cambio_imagen_tiempo:
             self.image_index = (self.image_index + 1) % len(self.images)
             self.tiempo_anterior_imagen = tiempo_actual
+    def puede_disparar(self):
+        return self.puede_disparar
+
+    def desactivar_disparo(self):
+        self.puede_disparar = False
+
+    def activar_disparo(self):
+        self.puede_disparar = True
+class Background:
+    def __init__(self, velocidad, num_imagenes=3):
+        self.imagenes = [pygame.image.load(f"background{i}.jpg") for i in range(1, num_imagenes + 1)]
+        self.velocidad = velocidad
+        self.num_imagenes = num_imagenes
+        self.imagen_actual = 0
+
+        # Redimensionar las imágenes al tamaño de la pantalla
+        pantalla_info = pygame.display.Info()
+        self.ancho_pantalla = pantalla_info.current_w
+        self.alto_pantalla = pantalla_info.current_h
+        self.imagenes = [pygame.transform.scale(imagen, (self.ancho_pantalla, self.alto_pantalla)) for imagen in self.imagenes]
+
+        self.pos_y = 0  # Posición inicial en el eje Y
+
+    def actualizar(self):
+        # Actualizar la posición vertical del fondo hacia abajo
+        self.pos_y += self.velocidad
+
+        # Si la imagen actual ha pasado completamente fuera de la pantalla hacia abajo, reiniciar la posición y cambiar la imagen
+        if self.pos_y >= self.alto_pantalla:
+            self.pos_y = 0
+            self.imagen_actual = (self.imagen_actual + 1) % self.num_imagenes
+
+    def dibujar(self, pantalla=None):
+        if pantalla is None:
+            pantalla = pygame.display.get_surface()
+
+        # Dibujar el fondo en dos posiciones para el desplazamiento continuo
+        pantalla.blit(self.imagenes[self.imagen_actual], (0, self.pos_y))
+
+        # Si la imagen se ha desplazado lo suficiente, dibujar la siguiente imagen hacia abajo
+        siguiente_imagen = (self.imagen_actual + 1) % self.num_imagenes
+        pantalla.blit(self.imagenes[siguiente_imagen], (0, self.pos_y - self.alto_pantalla))
+class Proyectil(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        super().__init__()
+        self.image = pygame.Surface((20, 20))  # Tamaño del proyectil
+        self.rect = self.image.get_rect()
+        self.rect.center = (x, y)
+
+        # Variables para el cambio de color constante
+        self.colores = [
+            (255, 0, 0),
+            (255, 165, 0),
+            (255, 255, 0),
+            (0, 255, 0),
+            (0, 0, 255),
+            (75, 0, 130),
+            (128, 0, 128)
+        ]
+        self.indice_color_actual = 0
+
+    def update(self):
+        # Actualizar la posición del proyectil hacia arriba
+        self.rect.y -= 5  # Puedes ajustar la velocidad cambiando este valor
+
+        # Cambiar de color constantemente
+        self.image.fill(self.colores[self.indice_color_actual])
+        self.indice_color_actual = (self.indice_color_actual + 1) % len(self.colores)
+
+        # Eliminar el proyectil si sale de la pantalla
+        if self.rect.bottom < 0:
+            self.kill()
--- Space Invaders/spaceinvaders.py
+++ Space Invaders/spaceinvaders.py
@@ -1,78 +1,76 @@
 import pygame
 from objetos import Cat
+from objetos import Background
+from objetos import Proyectil
 
 pygame.init()
 
 #Tamaño Pantalla
 
-pantalla = pygame.display.set_mode((1200, 600))
+pantalla = pygame.display.set_mode((1000, 600))
 
 #Titulo
 
 pygame.display.set_caption("Armada Alien Invasion Mexican Cat Who Is Really Just A Cat With A Sombrero And Maracas Defending His Chimoles From Aliens The Videogame")
 
-#Jugador
-
-cat = Cat()
-
-# Epilepsia Simulator
-
-colores = [
-    (255, 0, 0),    # Rojo
-    (255, 165, 0),  # Naranja
-    (255, 255, 0),  # Amarillo
-    (0, 255, 0),    # Verde
-    (0, 0, 255),    # Azul
-    (75, 0, 130),   # Índigo
-    (128, 0, 128)   # Violeta
-]
 #Tiempo/Ticks
 
-cambio_color_tiempo = 10  # Epilepsia block
+velocidad_fondo = 15    #Wow, me pregunto que hará esto
 cambio_imagen_tiempo = 150  # Jugador
-tiempo_anterior_color = pygame.time.get_ticks() #Cosas de epilepsia
 tiempo_anterior_imagen = pygame.time.get_ticks() #Cosas de frames
 image_index = 0
 indice_color_actual = 0
 clock = pygame.time.Clock()
 
+# Grupos de sprites
+todos_los_sprites = pygame.sprite.Group()
+proyectiles = pygame.sprite.Group()
+
 salir = False
+
+#Clases Importadas
+
+cat = Cat()
+background = Background(velocidad_fondo)
 
 #Juego
 
 while not salir:
-    pantalla.fill((0,0,0)) #Color pantalla
+    pantalla.fill((0,0,0))
+    background.actualizar()
+    background.dibujar(pantalla)
     # El juego
     
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             salir = True
+        elif event.type == pygame.KEYUP and event.key == pygame.K_SPACE:
+            cat.activar_disparo()  # Activar el disparo cuando se suelta la barra espaciadora
 
     #Movimiento        
             
     teclas = pygame.key.get_pressed()
     cat.mover(teclas)
 
-    # Cambio de color epileptico
+    #Tiempo
+
     tiempo_actual = pygame.time.get_ticks()
-    if tiempo_actual - tiempo_anterior_color >= cambio_color_tiempo:
-        indice_color_actual = (indice_color_actual + 1) % len(colores)
-        tiempo_anterior_color = tiempo_actual
-
-    #Rectangulo epileptico
-
-    rect_width = 80
-    rect_height = 80
-    rect_color = (colores[indice_color_actual])
-    rect = pygame.Rect((400 - rect_width // 2, 300 - rect_height // 2, rect_width, rect_height))
-    pygame.draw.rect(pantalla, rect_color, rect)
     
     #Jugador
     
     cat.cambiar_imagen(tiempo_actual, cambio_imagen_tiempo)
 
     cat.dibujar()
+    
+    # Crear un proyectil cuando se pulsa la tecla de espacio
+    if teclas[pygame.K_SPACE] and cat.puede_disparar:
+        proyectil = Proyectil(cat.posiz + cat.derecha.get_width() // 2, cat.postop)
+        todos_los_sprites.add(proyectil)
+        proyectiles.add(proyectil)
+        cat.desactivar_disparo()  # Desactivar el disparo cuando se presiona la barra espaciadora
 
+    todos_los_sprites.update()
+    todos_los_sprites.draw(pantalla)
     # No idea really
 
     pygame.display.flip()
